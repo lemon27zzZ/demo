3587caad9d5e70495dc237a0716e39b128be4af7
lemon <18055292560@163.com>
2018-12-06 16:22:38 +0800

    first commit

---
+++ demo2/src/main/java/action/demo/ActionApplication.java
@@ -0,0 +1,13 @@
+package action.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActionApplication {
+
+    @RequestMapping("/hi")
+    String hello(){
+        return "hi";
+    }
+}
